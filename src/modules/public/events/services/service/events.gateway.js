76727d71d6bd1d794db2009eab0254d22a81b99c
Cristian Campos <i20213tn003@utez.edu.mx>
2024-08-12 02:44:06 -0600

    [ADD] events consumptions

--- src/modules/public/events/services/service/events.gateway.js
+++ src/modules/public/events/services/service/events.gateway.js
@@ -1,58 +1,52 @@
-/* TODO  implement axios */
-
-import { events } from './data';
-
+import axios from "../../../../../config/client.gateway";
+import { events } from "./data";
 
 export default {
   async getEvents() {
     try {
-      const response = await Promise.resolve({ data: {data:events} });
+      const response = await axios.doGet("/events");
       return response.data.data;
-    }
-    catch (error) {
-      return Promise.reject(error);
+    } catch (error) {
+      return error;
     }
   },
   async saveEvent(event) {
     try {
-      const response = Promise.resolve({ data: {data:event[0]} });
+      const response = Promise.resolve({ data: { data: event[0] } });
       return response.data.data;
-    }
-    catch (error) {
+    } catch (error) {
       return Promise.reject(error);
     }
   },
 
   async deleteEvent(eventId) {
     try {
-      const response = Promise.resolve({ data: {data:events.filter(event => event.id !== eventId)} });
+      const response = Promise.resolve({
+        data: { data: events.filter((event) => event.id !== eventId) },
+      });
       return response.data.data;
-    }
-    catch (error) {
+    } catch (error) {
       return Promise.reject(error);
     }
   },
 
   async findEventById(eventId) {
     try {
-      const response = Promise.resolve({ data: {data:events.find(event => event.id === eventId)} });
+      const response = Promise.resolve({
+        data: { data: events.find((event) => event.id === eventId) },
+      });
       return response.data.data;
-    }
-    catch (error) {
+    } catch (error) {
       return Promise.reject(error);
     }
-  }, 
+  },
 
   async updateEvent(event) {
     try {
-      const response = Promise.resolve({ data: {data:event} });
+      const response = Promise.resolve({ data: { data: event } });
       return response.data.data;
-    }
-    catch (error) {
+    } catch (error) {
       return Promise.reject(error);
     }
-  }
-  
-
-
+  },
 };
