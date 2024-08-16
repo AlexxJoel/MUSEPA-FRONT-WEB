48328394c21735d8c5870d79a80a0aa901dc4126
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2024-08-15 19:42:02 -0600

    [ADD] getOne Events

--- src/modules/public/events/services/service/events.gateway.js
+++ src/modules/public/events/services/service/events.gateway.js
@@ -32,9 +32,7 @@ export default {
 
   async findEventById(eventId) {
     try {
-      const response = Promise.resolve({
-        data: { data: events.find((event) => event.id === eventId) },
-      });
+      const response = await axios.doGet(`/events/${eventId}`);
       return response.data.data;
     } catch (error) {
       return Promise.reject(error);
--- src/modules/public/events/views/DetailsEventView.vue
+++ src/modules/public/events/views/DetailsEventView.vue
@@ -2,11 +2,9 @@
   <div>
     <b-row class="px-5 pt-3 m-0">
       <b-col cols="6">
-
         <!-- Card -->
 
         <div class="p-4">
-
           <div class="d-flex justify-content-between mb-2">
             <h2>Información del evento</h2>
           </div>
@@ -14,37 +12,71 @@
           <b-form ref="form">
             <b-row class="pb-3">
               <b-col cols="6">
-                <b-form-group label="Nombre del evento" label-for="name" label-class="font-italic" label-size="sm">
+                <b-form-group
+                  label="Nombre del evento"
+                  label-for="name"
+                  label-class="font-italic"
+                  label-size="sm"
+                >
                   <p class="font-weight-bold h5">{{ event.name }}</p>
                 </b-form-group>
               </b-col>
               <b-col cols="6">
-                <b-form-group label="Categoria" label-for="category" label-class="font-italic" label-size="sm">
-                  <p class="font-weight-bold h5">{{ listCategories.find((c) => c.value === event.category).text }}</p>
+                <b-form-group
+                  label="Categoria"
+                  label-for="category"
+                  label-class="font-italic"
+                  label-size="sm"
+                >
+                  <p class="font-weight-bold h5">
+                    {{
+                     event.category
+                    }}
+                  </p>
                 </b-form-group>
               </b-col>
             </b-row>
 
             <b-row>
               <b-col cols="6">
-                <b-form-group label="Fecha de incio" label-for="startDate" ref="startDate" label-class="font-italic"
-                  label-size="sm">
-                  <p class="font-weight-bold h5">{{ formatDate(event.startDate) }}</p>
+                <b-form-group
+                  label="Fecha de incio"
+                  label-for="startDate"
+                  ref="startDate"
+                  label-class="font-italic"
+                  label-size="sm"
+                >
+                  <p class="font-weight-bold h5">
+                    {{ formatDate(event.start_date) }}
+                  </p>
                 </b-form-group>
               </b-col>
               <b-col cols="6">
-                <b-form-group label="Fecha de fin" label-for="endDate" ref="endDate" label-class="font-italic"
-                  label-size="sm">
-                  <p class="font-weight-bold h5">{{ formatDate(event.endDate) }}</p>
+                <b-form-group
+                  label="Fecha de fin"
+                  label-for="endDate"
+                  ref="endDate"
+                  label-class="font-italic"
+                  label-size="sm"
+                >
+                  <p class="font-weight-bold h5">
+                    {{ formatDate(event.end_date) }}
+                  </p>
                 </b-form-group>
               </b-col>
             </b-row>
 
-
             <b-row class="pb-3">
               <b-col cols="12">
-                <b-form-group label="Descripción" label-for="description" label-class="font-italic" label-size="sm">
-                  <p class="font-weight-bold h5">{{ event.description }}</p>
+                <b-form-group
+                  label="Descripción"
+                  label-for="description"
+                  label-class="font-italic"
+                  label-size="sm"
+                >
+                  <p class="font-weight-bold h5">
+                    {{ event.description }}
+                  </p>
                 </b-form-group>
               </b-col>
             </b-row>
@@ -54,44 +86,37 @@
                 Guardar evento
               </b-button>
             </div>
-
           </b-form>
         </div>
       </b-col>
       <b-col cols="6">
-
         <SectionDivider>
-          <template v-slot:title>
-            Imagenes
-          </template>
+          <template v-slot:title> Imagenes </template>
         </SectionDivider>
 
         <section class="p-4">
-          <VueSlickCarousel ref="c1" :asNavFor="$refs.c2"  v-bind="settingsCarousel">
-            <div
-              class="d-flex justify-content-center align-items-center"
-            ><img src="https://picsum.photos/300/300" /></div>
-
-            <div><img src="https://picsum.photos/300/300" /></div>
-
-            <div><img src="https://picsum.photos/300/300" /></div>
+          <VueSlickCarousel
+            ref="c1"
+            :asNavFor="$refs.c2"
+            v-bind="settingsCarousel"
+          >
+            <div v-for="(picture,index) in event.pictures" :key="index"  class="d-flex justify-content-center align-items-center">
+              <img :src="picture" alt="Imangen del evento" />
+            </div>
           </VueSlickCarousel>
         </section>
 
         <section>
-
-          <VueSlickCarousel ref="c2" :asNavFor="$refs.c1" :slidesToShow="2" 
-          :dots="true" 
-            v-bind="settingsCarousel">
-            <div><img src="https://picsum.photos/300/300"  height="100"/></div>
-            <div><img src="https://picsum.photos/300/300" height="100" /></div>
-            <div><img src="https://picsum.photos/300/300" height="100" /></div>
-
-
+          <VueSlickCarousel
+            ref="c2"
+            :asNavFor="$refs.c1"
+            :slidesToShow="2"
+            :dots="true"
+            v-bind="settingsCarousel"
+          >
+            <div v-for="(picture,index) in event.pictures" :key="index"><img :src="picture" height="100" /></div>
           </VueSlickCarousel>
-
         </section>
-
       </b-col>
     </b-row>
   </div>
@@ -99,23 +124,20 @@
 <script>
 import Vue from "vue";
 import eventsController from "../services/controller/events.controller";
-import { formatDate } from '../../../../kernel/moment';
-import vue2Dropzone from 'vue2-dropzone'
-import 'vue2-dropzone/dist/vue2Dropzone.min.css'
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import { formatDate } from "../../../../kernel/moment";
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
 // optional style for arrows & dots
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
-
-
-
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 export default Vue.extend({
   name: "SaveEventView",
   components: {
     VueSlickCarousel,
     vueDropzone: vue2Dropzone,
-    SectionDivider: () => import('@/components/SectionDivider.vue')
+    SectionDivider: () => import("@/components/SectionDivider.vue"),
   },
   data() {
     return {
@@ -131,15 +153,14 @@ export default Vue.extend({
         slidesToScroll: 1,
       },
 
-
       dropzoneOptions: {
-        url: 'https://httpbin.org/post',
+        url: "https://httpbin.org/post",
         thumbnailWidth: 150,
         maxFilesize: 0.5,
         headers: { "My-Awesome-Header": "header value" },
-        acceptedFiles: 'image/*',
+        acceptedFiles: "image/*",
         addRemoveLinks: true,
-        dictDefaultMessage: 'Arrastra tus imágenes aquí o haz clic para subir'
+        dictDefaultMessage: "Arrastra tus imágenes aquí o haz clic para subir",
       },
       dropZone: {
         useCustomSlot: true,
@@ -174,7 +195,6 @@ export default Vue.extend({
       },
       isLoading: false,
       listEvents: [],
-
       slide: 0,
       componentDragArea: {
         files: [],
@@ -189,34 +209,33 @@ export default Vue.extend({
         { value: 4, text: "Fiesta" },
       ],
       event: {
-        name: 'Fiesta de la primavera',
+        id: 0,
+        name: "Fiesta de la primavera",
+        description: "Fiesta de la primavera en el parque central",
+        startDate: "2021-12-10",
+        endDate: "2021-10-11",
         category: 4,
-        description: 'Fiesta de la primavera en el parque central',
-        startDate: '2021-10-10',
-        endDate: '2021-10-11',
         pictures: [
-          'https://musepa-bucket.s3.us-east-2.amazonaws.com/images/0e392eb4-43eb-470c-b31b-25c1807e33b5.jpg',
-          'https://musepa-bucket.s3.us-east-2.amazonaws.com/images/0e392eb4-43eb-470c-b31b-25c1807e33b5.jpg',
-          'https://musepa-bucket.s3.us-east-2.amazonaws.com/images/0e392eb4-43eb-470c-b31b-25c1807e33b5.jpg',
-        ]
+          "https://musepa-bucket.s3.us-east-2.amazonaws.com/images/0e392eb4-43eb-470c-b31b-25c1807e33b5.jpg",
+          "https://musepa-bucket.s3.us-east-2.amazonaws.com/images/0e392eb4-43eb-470c-b31b-25c1807e33b5.jpg",
+          "https://musepa-bucket.s3.us-east-2.amazonaws.com/images/0e392eb4-43eb-470c-b31b-25c1807e33b5.jpg",
+        ],
       },
-
     };
   },
   mounted() {
-    this.getListEvents();
+    this.getDeatilEvent();
 
     this.addFile();
   },
-  watch: {
-
-  },
+  watch: {},
   methods: {
-    async getListEvents() {
+    async getDeatilEvent() {
       try {
         this.isLoading = true;
-        const response = await eventsController.getEvents();
-        this.listEvents = response;
+        this.event.id = this.$route.params.id;
+        const response = await eventsController.findEventById(this.event.id);
+        this.event = response
       } catch (error) {
         console.error(error);
       } finally {
@@ -224,32 +243,27 @@ export default Vue.extend({
       }
     },
     validateFile(file) {
-
-
-
       // Verificar si el archivo ya ha sido subido
       if (this.event.pictures.find((p) => p.name === file.name)) {
-        alert('Este archivo ya ha sido subido.');
+        alert("Este archivo ya ha sido subido.");
         return;
       }
 
       // Agregar el archivo a la lista de subidos
-      console.log(file)
+      console.log(file);
       this.event.pictures.push(file);
     },
     handleRemoveFile(file) {
-      console.log('remove');
-      this.event.pictures = this.event.pictures.filter((p) => p.name !== file.name);
+      console.log("remove");
+      this.event.pictures = this.event.pictures.filter(
+        (p) => p.name !== file.name
+      );
     },
-    addFile() {
-
-
-    },
-    formatDate
+    addFile() {},
+    formatDate,
   },
 });
 </script>
-
 
 <style>
 .wallpaper {
@@ -269,9 +283,8 @@ h1 {
   font-size: 3rem;
 }
 
-
 .card {
-  background-color: #F2F2F2;
+  background-color: #f2f2f2;
 }
 
 .drag-area {
@@ -294,14 +307,11 @@ h1 {
 }
 
 .disabled-dropzone {
-
   pointer-events: none;
   /* Desactiva clics y arrastres */
   opacity: 0.7;
   /* Hace que parezca deshabilitado */
-
 }
-
 
 .slick-prev::before,
 .slick-next::before {
@@ -317,5 +327,4 @@ h1 {
   /* Flechas redondeadas */
   padding: 10px;
 }
-
 </style>
